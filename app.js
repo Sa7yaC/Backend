e044214a052540e0ed258c5434b0456e8c494dc4
Satya <sa7ya.30@gmail.com>
2025-09-25 22:48:18 +0530

    slight improvement

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+const app = express();
+
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
+}));
 
-app.use(express.json({limit: '20kb'}))
-app.use(express.urlencoded({ extended: true, limit: '20kb' }))
-app.use(express.static("public"))
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
 
-// //import routes
-import userRoutes from './src/routes/user.routes.js';
-// import adminRoutes from '../src/routes/admin.routes.js';
+
+// routes
+import userRoutes from "./src/routes/user.routes.js";
 
 app.use("/api/v1/user", userRoutes);
-// app.use("/api/v1/admin", adminRoutes);
+
 export { app };
